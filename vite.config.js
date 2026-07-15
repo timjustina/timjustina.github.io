ae0e7cc3e4bfa88834b6df0189e1aee3fc1e4a04
timjustina <timjustina@proton.me>
2026-07-15 20:53:56 +0200

    font edits

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [vue()],
-  base: '/'
-}))
+  base: '/',
+})
