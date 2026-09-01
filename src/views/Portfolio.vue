1ad567e84ee7e5a149dad0511a5c6b12aa44d3d7
timjustina <timjustina@proton.me>
2026-09-01 04:39:05 +0100

    about line length

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -4187,7 +4187,7 @@ export default {
     top: calc(140px - var(--about-gap));
     z-index: 2;
     width: var(--hero-squiggle-width);
-    height: var(--about-photo-h);
+    height: calc(var(--about-photo-h) - var(--about-line-bottom-trim));
     overflow: hidden;
     pointer-events: none;
 }
@@ -4763,6 +4763,7 @@ export default {
     --about-image-text-gap: clamp(32px, calc(32px + (100vw - 997px) * 32 / 457), 64px);
     --about-photo-w: clamp(281px, calc(281px + (100vw - 997px) * 12 / 457), 293px);
     --about-photo-h: clamp(402px, calc(402px + (100vw - 997px) * 18 / 457), 420px);
+    --about-line-bottom-trim: 17px;
     --about-photo-col-w: calc(2 * var(--about-image-text-gap) + var(--about-photo-w));
     --about-inner-max-w: clamp(800px, calc(800px + (100vw - 997px) * 187 / 457), 987px);
     --about-top-pad: 80px;
