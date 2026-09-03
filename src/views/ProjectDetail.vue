8911a381fa9027c8659878b404bd51ae0abb1825
timjustina <timjustina@proton.me>
2026-09-03 17:16:18 +0100

    top glass box UI

--- src/views/ProjectDetail.vue
+++ src/views/ProjectDetail.vue
@@ -385,6 +385,7 @@ export default {
     inset 0 1px 0 rgba(255, 255, 255, 0.75),
     inset 0 -1px 0 rgba(255, 255, 255, 0.3),
     0 6px 24px rgba(15, 23, 42, 0.05);
+  filter: drop-shadow(0 -6px 16px rgba(15, 23, 42, 0.1));
   -webkit-mask-image: linear-gradient(
     to bottom,
     rgba(0, 0, 0, 0.92) 0%,
